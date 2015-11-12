1c943157740dad276d28fde2a983f303eba69108
Blake Brown <blakebrown129@gmail.com>
2015-11-12 09:01:01 -0500

    Update naming scheme + projects section

--- js/script.js
+++ js/script.js
@@ -1,22 +1,14 @@
-// $(document).mousemove(function(e){
-//     $("#loader_circle").css({left:e.pageX, top:e.pageY});
-// });
-
+// $(window).load is fired after all content (including images) have been loaded
 $(window).load(function() {
-		// Animate loader off screen
-		//$("#loader").fadeTo(3000, 0);
-			
-				$("#loader").addClass("fade_out_animation");
-				$("#section_0").addClass("fade_in_animation");
-				setTimeout(function() {
-					$("#welcome").addClass("fade_in_animation");
-				}, 1000);
-			
-		// $("#loader").fadeTo("slow", 0, function() {
-		// 	$("#main_content").show();
-		// 	$("#main_content").fadeTo(0, 1);
-		// });
-	});
+	// Home screen animation, fades in background + name + sections
+	$("#section-0").addClass("fade-in-animation");
+	setTimeout(function() {
+		$("#welcome").addClass("slide-down-animation");
+		setTimeout(function() {
+			$("#brown").addClass("fade-in-animation");
+		}, 1000);
+	}, 1000);
+});
 // $(window).scroll(function() {
 // 	console.log("hello");
 //      if($(window).scrollTop() > 0) {
