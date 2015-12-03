42c3fda3a27d44bf0a54336991527b9cd018a124
Blake Brown <blakebrown129@gmail.com>
2015-12-03 01:33:21 -0500

    Progress #6: Work on intro section

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,6 +8,18 @@ $(window).load(function() {
 			$('#brown').addClass('fade-in-animation');
 		}, 1000);
 	}, 1000);
+
+	// Change the text
+	var date = new Date();
+	var localHour = date.getHours();
+	if(localHour >= 0 && localHour <= 5) {
+		$('#intro-header').text('Bonne nuit!');
+	} else if(localHour >= 6 && localHour <= 10) {
+		$('#intro-header').text('Bon matin!');
+	} else if(localHour >= 19 && localHour <= 23) {
+		$('#intro-header').text('Bonsoir!');
+	} 
+
 });
 
 
