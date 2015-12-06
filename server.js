ddee8713e199b56f56c263ec73ac404208457747
Blake Brown <blakebrown129@gmail.com>
2015-12-05 20:19:31 -0500

    Progress #8: Improve responsiveness & modal descriptions, initialize side projects on seperate ports on server startup

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 var express     = require('express'),
 	app         = express(), 
-	http = require('http'),
-	httpProxy = require('http-proxy');
+	fork = require('child_process').fork;
 
 // Sets the directory path for the app, __dirname resolves to the directory where this script resides
 app.use(express.static(__dirname));
@@ -11,11 +10,13 @@ app.get('/', function(req,res) {
 	res.sendFile('index.html');
 });
 
-// // Get hackroulette
-// app.get('/hackroulette', function(req,res) {
-// 	console.log("Proxying to hackroulette");
-// 	proxy.web(req, res, { target: 'http://localhost:3001' });
-// });
-
+// Start node app
 app.listen(3000);
-console.log("Listening on port 3000");
+console.log("Personal Website listening on port 3000");
+
+// Side projects! 
+// Use node child processes to start side projects on different ports
+// Then all we need is nginx to act as a proxy to redirect blakelockbrown.com/hackroulette to localhost:3001
+// Plain and simple :) 
+var hackrouletteApp = fork('projects/hackroulette/server.js');
+var onlineCompilerApp = fork('projects/Online-Compiler/server.js');
