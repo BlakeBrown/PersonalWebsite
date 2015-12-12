a572bc8ede1b5461ca56e59ce96f69e129220e03
Blake Brown <blakebrown129@gmail.com>
2015-12-11 21:01:03 -0500

    Progress #11: Work on front end, add typed.js, rename sections

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,24 +20,50 @@ $(window).load(function() {
 	// 	$('#intro-header').text('Bonsoir!');
 	// } 
 
-	// $.firefly({
-	// 	color: '#fff',
-	// 	minPixel: 1,
-	// 	maxPixel: 3,
-	// 	total : 50,
-	// 	on: 'section-0'
-	// });
+	$.firefly({
+		color: '#fff',
+		minPixel: 1,
+		maxPixel: 3,
+		total : 50,
+		on: 'section-0'
+	});
 
+	var seenIntroHeader = false;
 
 	// Fade out section-0 on window scroll
-	$(window).scroll(function () {
+	$(window).on('scroll', function () {
 	    var scrollTop = $(window).scrollTop();
-	    var height = $(window).height();
+	    var height = $('#section-0').height();
 	    $('#section-0').css({
 	        'opacity': ((height - scrollTop) / height)
 	    }); 
+	    if(!seenIntroHeader) {
+	    	if(isScrolledIntoView('#intro-header')) {
+				seenIntroHeader = true;
+				$("#intro-header").addClass('fadeInUp');
+	    	}
+	    	// $("#intro-header").typed({
+	    	//     strings: ["Hello World"],
+	    	//     typeSpeed: 50,
+	    	//     startDelay: 1000
+	    	// });
+	    }
 	});
 
+	// Returns true if element is scrolled into view
+	function isScrolledIntoView(elem)
+	{
+	    var $elem = $(elem);
+	    var $window = $(window);
+
+	    var docViewTop = $window.scrollTop();
+	    var docViewBottom = docViewTop + $window.height();
+
+	    var elemTop = $elem.offset().top;
+	    var elemBottom = elemTop + $elem.height();
+
+	    return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+	}
 });
 
 
